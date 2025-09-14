cc65674735a767cc492ac696376889f2b29d6791
Minsung Kang <minsungkang2012@gmail.com>
2025-09-15 01:05:09 +0900

    1. Used useState hook for managing uploaded file, 2. created input of file type for file upload, 3. Added onChange attrib/prop and assigned event handler handleFileChange which updates state and prints file, 4.  Added conditional assignment logic for file to prevent error from selecting empty file + added condition rendering to prevent error from selecting file from null state + added types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  // useState는 맨 처음에
+  // because proper inference from initial value is not possible, add type to useState which provides generic type 
+  const [selectedFile, setSelectedFile] = useState<File | null>(null); 
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
+    const file = event.target.files && event.target.files.length > 0
+                  ? event.target.files[0]
+                  : null;
+    setSelectedFile(file);
+    console.log("File selected", file);
+  };
+
   return (
     <div className="app-container">
-      <h1>Minsung</h1>
-      <p>File Upload Application</p>
+      <h1>Minsung File Upload Application</h1>
+          <input type="file" id="file-input" 
+              onChange={handleFileChange}
+          />
+          <label htmlFor="file-input">  {/*htmlFor connects to id of input*/}
+            Choose File
+          </label>
+
+          {selectedFile && (  // prevent error when selectedFile is null (.name on null NONO)
+           <p>Selected: {selectedFile.name}</p>
+          )}
     </div>
   );
 }
