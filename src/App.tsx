31494b94f5abcaf875a3ca6116a1cff778a65599
Minsung Kang <minsungkang2012@gmail.com>
2025-09-15 00:02:29 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Container, Box } from "@mui/material";
+
+export default function App() {
+  return (
+    <Box sx={{ flexGrow: 4 }}>
+      <Container>
+        <h1>HACKERSHRINE</h1>
+        <p>File Upload Application</p>
+      </Container>
+    </Box>
+  );
+}
